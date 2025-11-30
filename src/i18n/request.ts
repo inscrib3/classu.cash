391a754db4b0322f31f8e5536c6c5b36ca988813
thewrlck <thewrlck@proton.me>
2025-11-30 11:06:43 +0100

    try relative import

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -17,6 +17,6 @@ export default getRequestConfig(async ({requestLocale}) => {
     
   return {
     locale,
-    messages: (await import(`@/translations/${locale}.json`)).default
+    messages: (await import(`../../translations/${locale}.json`)).default
   };
 });
